c3dca7c2539be9154014eaa921088b0866ffe87b
Samuel Trahan <Samuel.Trahan@noaa.gov>
2024-08-28 14:03:51 -0400

    WaterInTheWest.sh: default to 6hr C768 128 level

--- rocoto/WaterInTheWest.sh
+++ rocoto/WaterInTheWest.sh
@@ -8,11 +8,11 @@ set -x
 cd ${HOMEhafs}/rocoto
 EXPT=$(basename ${HOMEhafs})
 SUBEXPT=$EXPT
-conf=../parm/WaterInTheWest.conf
+conf='../parm/WaterInTheWest.conf ../parm/WaterInTheWestL128C768.conf'
 opts="-t -f"
 
 scrubopt="config.scrub_work=no config.scrub_com=no"
 ./run_hafs.py ${opts} 2023022400 00L HISTORY \
     config.EXPT=${EXPT} config.SUBEXPT=${SUBEXPT} \
-    config.NHRS=24 ${scrubopt} \
+    config.NHRS=6 ${scrubopt} \
     $conf config.run_pygraf=yes
