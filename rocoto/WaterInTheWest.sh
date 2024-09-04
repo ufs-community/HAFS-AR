ba145dd044c60cdbc825628aff14d17a704be484
Samuel Trahan <Samuel.Trahan@noaa.gov>
2024-09-04 19:48:07 -0400

    WaterInTheWest.sh: update instructions

--- rocoto/WaterInTheWest.sh
+++ rocoto/WaterInTheWest.sh
@@ -5,6 +5,19 @@ HOMEhafs=$( cd $( dirname ${BASH_SOURCE[0]} ) ; cd .. ; pwd -P )
 source ${HOMEhafs}/ush/hafs_pre_job.sh.inc
 set -x
 
+# NOTE: If you're running more than 24 hours, increase the wallclock limit in the rocoto/sites files.
+#
+# Hera: rocoto/sites/hera.ent
+# GAEA C5: rocoto/sites/gaea_c5.ent
+#
+# Change the FORECAST_WALLTIME entity line:
+#
+#    <!ENTITY FORECAST_WALLTIME "<walltime>02:59:00</walltime>">
+#
+# To this:
+#
+#    <!ENTITY FORECAST_WALLTIME "<walltime>07:59:00</walltime>">
+
 cd ${HOMEhafs}/rocoto
 EXPT=$(basename ${HOMEhafs})
 SUBEXPT=${EXPT}
@@ -14,11 +27,15 @@ scrubopt="config.scrub_work=no config.scrub_com=no"
 # Our target configuration, C768 128-level nested tile on a rotated stretched sphere:
 conf='../parm/WaterInTheWest.conf ../parm/WaterInTheWestC768.conf ../parm/WaterInTheWestL128C768.conf'
 
-# NOTE: If you're running 24 hours or less, consider reducing the wallclock limit.
-# In rocoto/sites/gaea_c5.ent or rocoto/sites/hera.ent, change the walltime line:
-#    <!ENTITY FORECAST_WALLTIME "<walltime>07:59:00</walltime>">
-# Three hours should be enough for short forecasts.
-#    <!ENTITY FORECAST_WALLTIME "<walltime>02:59:00</walltime>">
+# See below for alternative conf= lines for other configurations.
+
+# This command generates the workflow xml file and runs rocotorun.
+./run_hafs.py ${opts} 2023022400 00L HISTORY \
+    config.EXPT=${EXPT} config.SUBEXPT=${SUBEXPT} \
+    config.NHRS=6 ${scrubopt} \
+    $conf
+
+# ---
 
 # Alternative configurations
 
@@ -42,9 +59,3 @@ conf='../parm/WaterInTheWest.conf ../parm/WaterInTheWestC768.conf ../parm/WaterI
 
 # C768 128-level uniform sphere (like GFS)
 # conf='../parm/WaterInTheWest.conf ../parm/WaterInTheWestC768.conf ../parm/WaterInTheWestL128C768.conf ../parm/WaterInTheWestC768NoNest.conf'
-
-# Run run_hafs.py to generate the workflow xml file and runs the workflow.
-./run_hafs.py ${opts} 2023022400 00L HISTORY \
-    config.EXPT=${EXPT} config.SUBEXPT=${SUBEXPT} \
-    config.NHRS=6 ${scrubopt} \
-    $conf
