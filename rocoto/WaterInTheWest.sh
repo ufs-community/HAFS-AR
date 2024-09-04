2be29bcfb0daacc571d210a1519931dc389911ee
Samuel Trahan <Samuel.Trahan@noaa.gov>
2024-09-04 17:37:35 -0400

    instructions on how to reduce wallclock limit

--- rocoto/WaterInTheWest.sh
+++ rocoto/WaterInTheWest.sh
@@ -31,7 +31,13 @@ conf='../parm/WaterInTheWest.conf ../parm/WaterInTheWestC768.conf ../parm/WaterI
 # # C384 128-level uniform sphere (like GFS)
 # conf='../parm/WaterInTheWest.conf ../parm/WaterInTheWestC384.conf ../parm/WaterInTheWestL128.conf ../parm/WaterInTheWestC384Uniform.conf'
 
-# This script generates the workflow xml file and runs the workflow.
+# NOTE: If you're running 24 hours or less, consider reducing the wallclock limit.
+# In rocoto/sites/gaea_c5.ent or rocoto/sites/hera.ent, change the walltime line:
+#    <!ENTITY FORECAST_WALLTIME "<walltime>07:59:00</walltime>">
+# Three hours should be enough for short forecasts.
+#    <!ENTITY FORECAST_WALLTIME "<walltime>02:59:00</walltime>">
+
+# Run run_hafs.py to generate the workflow xml file and runs the workflow.
 ./run_hafs.py ${opts} 2023022400 00L HISTORY \
     config.EXPT=${EXPT} config.SUBEXPT=${SUBEXPT} \
     config.NHRS=6 ${scrubopt} \
